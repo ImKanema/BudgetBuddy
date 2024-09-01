d9fea13673b21a61c35219099f813d718eee29e9
Kanema <achmadyoungky3@gmail.com>
2024-09-01 15:56:15 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded',function(){
+const toggler = document.querySelector('.navbar-toggler');
+const icon = toggler.querySelector('.navbar-toggler-icon');
+
+icon.innerHTML = '<span style="font-size: 24px;">&#x2630;</span>';
+
+function updateIcon(){
+	if (toggler.classList.contains('collapsed')) {
+		icon.innerHTML = '<span style="font-size: 24px;">&#x2630;</span>';
+	}else{
+		icon.innerHTML = '<span style="font-size: 24px;">&#x00d7;</span>';
+	}
+};
+
+toggler.addEventListener('click', function(){
+	updateIcon();
+	});
+});
+
+// #######
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+        const middleOfViewport = (window.innerHeight / 2) - (targetElement.offsetHeight / 2);
+
+        window.scrollTo({
+            top: targetPosition - middleOfViewport,
+            behavior: 'smooth'
+        });
+    });
+});
+
+function ScrolltoTop(){
+	window.scrollTo({
+		top:0,
+		behavior: 'smooth'
+	});
+}
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Get the button and modal elements
+//     var openModalButton = document.getElementById('openModalButton');
+//     var bukaModal = document.getElementById('bukaModal');
+//     var myModal = new bootstrap.Modal(document.getElementById('myModal'));
+
+//     // Add event listener to the button
+//     openModalButton.addEventListener('click', function () {
+//       myModal.show(); // Show the modal
+//     });
+
+//     bukaModal.addEventListener('click', function () {
+//       myModal.show(); // Show the modal
+//     });
+//   });
+
+function scrollToFeatures() {
+    const element = document.getElementById('features');
+    const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+    const offset = window.innerHeight / 6; // Offset to center the element
+
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
+  }
+
+  function scrollToHarga() {
+    const element = document.getElementById('harga');
+    const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+    const offset = window.innerHeight / 6; // Offset to center the element
+
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
+  }
+
+  // ############
